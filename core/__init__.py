a11e00dbb9be2b9c2515598c279690e45c0a3854
Max Mischinger <maxmischinger@icloud.com>
2021-05-19 11:43:59 +0200

    update requiremtns.txt
    this is a pip freeze after installing all dependencies including versions.

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,7 @@
-pass
+from flask import Flask
+from core.trape import Trape
+from core.db import Database
+
+trape = Trape()
+db = Database()
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
