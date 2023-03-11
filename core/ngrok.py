316cba861c25366ea8fc1293728f2fbff0515021
Robert Jones <rbrt.jns90@gmail.com>
2023-03-11 15:42:14 -0500

    Update ngrok.py

--- core/ngrok.py
+++ core/ngrok.py
@@ -63,7 +63,7 @@ class ngrok(object):
 			saved_file.write(download.read())
 			saved_file.close()
 			
-			if platform.release() < 10:
+			if int(platform.release()) < 10:
 				result = subprocess.check_output(["unzip", filename])
 			else:
 				result = subprocess.check_output(['tar', '-xf', filename, str_ngrok])
